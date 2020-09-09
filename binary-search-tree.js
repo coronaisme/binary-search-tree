c41557249e199becc1055b49f6a893d0a7501a39
coronaisme <coronaisme@gmail.com>
2020-09-09 15:23:40 -0700

    tree added

---
+++ binary-search-tree.js
@@ -0,0 +1,16 @@
+class Node {
+
+  constructor({val, left=null, right=null}) {
+    this.val = val
+    this.left = left
+    this.right = right
+  }
+
+}
+
+class Tree {
+
+  constructor(rootNode) {
+    this.rootNode = rootNode
+  }
+}
